bebf8502599f3c29b9e45216f07e07743f32eb33
biro.vince <quick.biro@gmail.com>
2020-05-14 22:44:45 +0200

    file upload fix 2.6

--- middlewares/user/saveProfileDataMW.js
+++ middlewares/user/saveProfileDataMW.js
@@ -9,7 +9,7 @@ module.exports = function (objectrepository) {
         const pass1 = req.body.pass1 ? req.body.pass1 : null;
         const pass2 = req.body.pass2 ? req.body.pass2 : null;
         const img = req.file ? req.file : null;
-        const imgPath = img ? img.path : null;
+        const imgPath = img ? img.path.slice(5) : null;
         const city = req.body.city ? req.body.city : null;
         const price = req.body.price ? req.body.price : null;
 
