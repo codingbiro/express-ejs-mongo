4ee369b28ab26f5bf3337e9e7a051134dfacc947
biro.vince <quick.biro@gmail.com>
2020-05-14 22:20:12 +0200

    file upload fix 2.0

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'assets/images');
+        cb(null, '/assets/images');
     },
     filename: (req, file, cb) => {
         cb(null, new Date().toISOString() + '_' + file.originalname);
