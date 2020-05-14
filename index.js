03681ef2004a437b6332d9c023fa993a8b68cc30
biro.vince <quick.biro@gmail.com>
2020-05-14 16:01:54 +0200

    deployment

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('express-flash-messages');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 // Using EJS templating
 app.set('view engine', 'ejs');
@@ -13,6 +18,16 @@ app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Helmet for security
+app.use(helmet());
+
+// Compression for optimizing page size
+app.use(compression());
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+// Morgan for logs
+app.use(morgan('combined', { stream: accessLogStream }));
+
 // Flash messages
 app.use(flash());
 app.use(function (req, res, next) {
@@ -22,6 +37,9 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Connecting to MongoDB
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_MATECHUS}:${process.env.MONGODB_MATECHPW}@matech0-w7nyo.mongodb.net/${process.env.MONGODB_DEF_DB}`;
+
 // Static folder
 app.use(express.static('assets'));
 
@@ -29,7 +47,7 @@ app.use(express.static('assets'));
 require('./routes/index')(app);
 
 // Port settings
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Start server
 const server = app.listen(PORT, function () {
