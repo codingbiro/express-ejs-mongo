5480f438e09b8539d3d8d707cb6f297ae0aafe5b
biro.vince <quick.biro@gmail.com>
2020-05-30 15:22:47 +0200

    bugf

--- config/db.js
+++ config/db.js
@@ -4,5 +4,6 @@ const mongoose = require('mongoose');
 const MONGODB_URI = process.env.NODE_ENV ? `mongodb+srv://${process.env.MONGODB_MATECHUS}:${process.env.MONGODB_MATECHPW}@matech0-w7nyo.mongodb.net/${process.env.MONGODB_DEF_DB}` : 'mongodb://localhost:27017/matech_testenv';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 
 module.exports = mongoose;
