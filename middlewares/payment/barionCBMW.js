d162e081350a6503da23b3a8005164299f9711f7
biro.vince <quick.biro@gmail.com>
2020-05-30 11:58:07 +0200

    Cb bugfix

--- middlewares/payment/barionCBMW.js
+++ middlewares/payment/barionCBMW.js
@@ -14,10 +14,9 @@ module.exports = function (objectrepository) {
         let theid = 0;
         if (req.query.paymentId !== undefined) theid = req.query.paymentId;
 
-        await axios.get(BASE_URL + STATE, {
-            "POSKey": PRIVATE_POS_KEY,
-            "PaymentId": theid
-        }).then(function (response) {
+        const PARAMS = `?POSKey=${PRIVATE_POS_KEY}&PaymentId=${theid}`;
+
+        await axios.get(BASE_URL + STATE + PARAMS).then(function (response) {
             if (response) {
                 orderModel.updateOne({ pid: theid },
                     {
