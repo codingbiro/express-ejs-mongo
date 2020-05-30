b2220e6bcd43d010acdb89405e099022153d16e7
biro.vince <quick.biro@gmail.com>
2020-05-30 23:06:42 +0200

    a

--- middlewares/user/getProfileApplicationsMW.js
+++ middlewares/user/getProfileApplicationsMW.js
@@ -39,8 +39,12 @@ module.exports = function (objectrepository) {
                 }
                 theOrder = order;
             });
-
-            theMsgs.push({ uid: theUser._id, name: theUser.name, email: theUser.email, img: theUser.img ? theUser.img : 'assets/avatar-placeholder.gif', time: time, lid: anApp.lid, state: theOrder != null ? theOrder.state : null });
+            console.log(theOrder);
+            let theState;
+            if (theOrder == null) theState = '';
+            else theState = theOrder.state;
+            console.log(theState);
+            theMsgs.push({ uid: theUser._id, name: theUser.name, email: theUser.email, img: theUser.img ? theUser.img : 'assets/avatar-placeholder.gif', time: time, lid: anApp.lid, state: theState });
         }
 
         res.locals.messages = theMsgs;
