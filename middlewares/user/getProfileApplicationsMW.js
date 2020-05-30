bd80e5bd28f3524bc099a95fd490a508f771fc9b
biro.vince <quick.biro@gmail.com>
2020-05-30 16:57:33 +0200

    y

--- middlewares/user/getProfileApplicationsMW.js
+++ middlewares/user/getProfileApplicationsMW.js
@@ -8,7 +8,7 @@ module.exports = function (objectrepository) {
     return async function (req, res, next) {
         let theApps = [];
         console.log(req.session.userMail);
-        await userModel.findOne({ email: req.session.userMail }, (err, user) => {
+        await userModel.findOne({ email: req.session.userMail }, async (err, user) => {
             if (err) {
                 req.session.sessionFlash = {
                     type: 'danger',
