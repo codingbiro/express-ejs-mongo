301f8f96651cf0a8bb2916a20fd3d942a85ace51
biro.vince <quick.biro@gmail.com>
2020-05-31 21:26:07 +0200

    Mail config

--- middlewares/auth/resetPassMW.js
+++ middlewares/auth/resetPassMW.js
@@ -44,13 +44,13 @@ module.exports = function (objectrepository) {
                     port: 587,
                     secure: false, // upgrade later with STARTTLS
                     auth: {
-                        user: process.env.NODE_ENV ? process.env.MG_USER : '',
-                        pass: process.env.NODE_ENV ? process.env.MG_MATH_BIROVINCE_P : ''
+                        user: process.env.NODE_ENV ? process.env.MG_USER_2 : '',
+                        pass: process.env.NODE_ENV ? process.env.MG_BIRO_WTF_P : ''
                     }
                 });
 
                 const mailOptions = {
-                    from: 'reset@birovince.com',
+                    from: 'iforgot@biro.wtf',
                     to: theUser.email,
                     subject: 'Password reset',
                     text: 'Click this magic link to reset your password: ' + url
